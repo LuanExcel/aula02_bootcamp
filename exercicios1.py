36a632ccd38eeecfa96da99a446c7a31c7c601da
Luan <luanr@ymail.com>
2024-07-08 05:04:12 -0300

    adidionado exercicio 10

--- exercicios1.py
+++ exercicios1.py
@@ -1,9 +1,23 @@
-nome = 'luan@rodrigues'
+import math
 
+#Separador
+nome = 'luan@rodrigues'
 print(nome.split('@'))
 
-
+#Divisao inteira
 numero1 = int(input("Inserir numero inteiro: "))
 numero2 = int(input("Inserir numero inteiro: "))
 resultado = numero1 // numero2
 print(resultado)
+
+#cauclo do raio com duas casas decimais
+raio_circulo = float(input("digite o raio: "))
+area_circulo = math.pi * raio_circulo ** 2
+
+#metodo antigo
+area_circulo_formatada = "{:.2f}".format(area_circulo)
+print(area_circulo_formatada)
+
+#novo
+print(f"{area_circulo:.2f}" )
+
