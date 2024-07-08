ffa32a6a991905ac40cdda2edccb3768c60bdbe4
Luan <luanr@ymail.com>
2024-07-08 04:45:01 -0300

    Exercico 01

---
+++ exercicios1.py
@@ -0,0 +1,9 @@
+nome = 'luan@rodrigues'
+
+print(nome.split('@'))
+
+
+numero1 = int(input("Inserir numero inteiro: "))
+numero2 = int(input("Inserir numero inteiro: "))
+resultado = numero1 // numero2
+print(resultado)
