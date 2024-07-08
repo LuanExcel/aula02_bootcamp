fe28ef2cf26ad9315c2f7902676f5a58a29ffa60
Luan <luanr@ymail.com>
2024-07-08 04:37:47 -0300

    adicionado exercico 04

---
+++ exercicios2.py
@@ -0,0 +1,13 @@
+print(f'Adicao: {1+1}')
+
+print(f'Subtracao: {2-1}')
+
+print(f'Multiplicacao: {2*1}')
+
+print(f'Divisao Normal: {2 / 1}')
+
+print(f'Divisao Inteira: {2 // 1}')
+
+print(f'Resto da Divisão: {6 % 4}')
+
+print(f'Potencia: {2 ** 2}')
